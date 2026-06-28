e11a54a29f18da2b09b25a2d75f603faf12c74be
abhay92141 <abhaygupta92006@gmail.com>
2026-06-28 20:07:19 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH")
+MONGO_URI = os.getenv("MONGO_URI")
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "")
